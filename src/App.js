165933232a4895b4069ee87689eaf37d06e8efc7
sedx876 <swatkins876@gmail.com>
2021-02-01 11:25:41 -0500

    code update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>BubbleChat</h1>
+      {/* <h1>BigRatChat</h1> */}
+      <ChatEngine
+			height='100vh'
+			userName='admin'
+			userSecret='123456'
+      projectID='790612d8-1386-4a3e-896c-199330f71e0b'
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+		  />
     </div>
   );
 }
 
-export default App;
+export default App
